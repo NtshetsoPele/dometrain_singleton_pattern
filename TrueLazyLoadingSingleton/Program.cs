a6510be0af8e583f81a430adb48b41e29a184139
NtshetsoPele <mothobi.bald@gmail.com>
2024-09-24 17:20:01 +0200

    True Lazy Loading Singleton Implementation

---
+++ TrueLazyLoadingSingleton/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine(TrueLazyLoadingSingleton.ClassName);
+
+ParallelEnumerable
+    .Range(start: 0, count: 10_000)
+    .ForAll(action: _ =>
+    {
+        var __ = TrueLazyLoadingSingleton.Instance;
+    });
+
+Console.WriteLine("Goodbye, World!");
+
+internal sealed class TrueLazyLoadingSingleton
+{
+    public static string ClassName;
+    public static TrueLazyLoadingSingleton Instance => Nested.Instance;
+
+    private TrueLazyLoadingSingleton() => Console.WriteLine("Instantiated.");
+
+    static TrueLazyLoadingSingleton() => ClassName = nameof(TrueLazyLoadingSingleton);
+
+    private class Nested
+    {
+        internal static readonly TrueLazyLoadingSingleton Instance;
+
+        // 'beforefieldinit' removed from IL
+        static Nested() => Instance = new();
+    }
+}
